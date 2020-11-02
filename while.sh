6cfbc6195fd7d238f1bac23443ec849de82c9f13
JacobKelly11 <71235791+JacobKelly11@users.noreply.github.com>
2020-11-01 19:16:24 -0600

    Update while.sh

--- while.sh
+++ while.sh
@@ -4,7 +4,7 @@
 #code looks clean, I would probably space "counter += 1" just to make it easier to read.
 
 #while counter < 3, counter increases by 1, echo counter and repeat loop.
-counter=0
+counter = 0
 while [ $counter -lt 3 ]; do
     let counter += 1
     echo $counter
