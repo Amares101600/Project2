344f1338a32a0031f1e3754884c4c60ce3e042df
JacobKelly11 <71235791+JacobKelly11@users.noreply.github.com>
2020-11-01 18:52:12 -0600

    Update if_else.sh

--- if_else.sh
+++ if_else.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+#code compares two integer values using an if/then/else statement and prints if it is less than or greater than.
+#I like the placement and setup, very clean and easy to read.
+
+#initialize integer variables
 num_a=800
 num_b=200
 
+#if num_a < num_b then print "num_a is less than num_b!" else print "num_a is greater than num_b!"
 if [ $num_a -lt $num_b ]; then
     echo "$num_a is less than $num_b!"
 else
