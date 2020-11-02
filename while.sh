6918430dd6885857c1a44babeed3db353e1a00e8
JacobKelly11 <71235791+JacobKelly11@users.noreply.github.com>
2020-11-01 18:59:15 -0600

    Update while.sh

--- while.sh
+++ while.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+#code tests if counter is less than 3, let counter increase by one and repeat while until greater than or equal to 3.
+#code looks clean, I would probably space "counter += 1" just to make it easier to read.
+
+#while counter < 3, counter increases by 1, echo counter and repeat loop.
 counter=0
 while [ $counter -lt 3 ]; do
-    let counter+=1
+    let counter += 1
     echo $counter
 done
