53dfc902f4bdbb9a1f7194d6dd3188d547f15360
JacobKelly11 <71235791+JacobKelly11@users.noreply.github.com>
2020-11-01 19:03:59 -0600

    Update until.sh

--- until.sh
+++ until.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#Code tests that until counter is less than 3, let counter decrease by one, echo counter, and repeat loop.
+#Code looks clean but I would space "counter -= 1" to make it cleaner.
+
 counter=6
 until [ $counter -lt 3 ]; do
     let counter-=1
